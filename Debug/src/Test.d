74d5d2ffafb5da2e8ace806bc125ce94b52d6f63
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-03-31 17:15:39 +0200

    initial commit

---
+++ Debug/src/Test.d
@@ -0,0 +1,73 @@
+src/Test.o: ../src/Test.cpp \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_base.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_to_string.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_demangle.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_determine_version.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_equals.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_diff_values.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_determine_traits.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_relops.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_data_driven.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_throws.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_suite.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_test.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_determine_library.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_repeated_test.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_suite_test.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_test_incarnate.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_testmember.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_version.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/ide_listener.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_listener.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/xml_listener.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_xml_file.h \
+  /Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_runner.h
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_base.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_to_string.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_demangle.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_determine_version.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_equals.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_diff_values.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_determine_traits.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_relops.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_data_driven.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_throws.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_suite.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_test.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_determine_library.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_repeated_test.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_suite_test.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_test_incarnate.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_testmember.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_version.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/ide_listener.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_listener.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/xml_listener.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_xml_file.h:
+
+/Users/hirsch/Documents/fhnw/prcpp/my-string/MyString/cute/cute_runner.h:
