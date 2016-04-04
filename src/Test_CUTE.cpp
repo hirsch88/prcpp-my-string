c717fb58a98c8fd483065292b9711e994cc4b61a
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-04-04 12:05:27 +0200

    adds some more tests

--- src/Test_CUTE.cpp
+++ src/Test_CUTE.cpp
@@ -11,10 +11,10 @@
 
 #include "MyString.h"
 
-void test_length(){
-	ASSERT(MyString().length() == 0);
-	ASSERT(MyString("").length() == 0);
-	ASSERT(MyString("abc").length() == 3);
+void test_length() {
+    ASSERT(MyString().length() == 0);
+    ASSERT(MyString("").length() == 0);
+    ASSERT(MyString("abc").length() == 3);
 }
 
 //void test_toCString(){
@@ -24,49 +24,49 @@ void test_length(){
 //	ASSERT_EQUAL(MyString(s2.toCString().get()), s2);
 //}
 
-//void test_equals(){
-//    MyString s("abc");
-//	ASSERT(MyString() == MyString(""));
-//	ASSERT(MyString("") == MyString());
-//	ASSERT(s ==  s);
-//	ASSERT(!(s == MyString("ab")));
-//}
+void test_equals() {
+    MyString s("abc");
+    ASSERT(MyString() == MyString(""));
+    ASSERT(MyString("") == MyString());
+    ASSERT(s == s);
+    ASSERT(!(s == MyString("ab")));
+}
 
-//void test_compare(){
-//    MyString s("abc");
-//	ASSERT_EQUAL(MyString().compareTo(""), 0);
-//	ASSERT(s.compareTo("bcd") < 0);
-//	ASSERT(s.compareTo("abde") < 0);
-//	ASSERT(s.compareTo("abcd") < 0);
-//	ASSERT(s.compareTo("abbb") > 0);
-//	ASSERT(s.compareTo("ABC") > 0);
-//	ASSERT(s.compareTo("") > 0);
-//	ASSERT(MyString().compareTo(s) < 0);
-//}
-//
-//void test_concat(){
-//    MyString s;
-//	ASSERT(s.concat("") == "");
-//	ASSERT(s.concat('a') == "a");
-//	ASSERT(MyString("a").concat("") == "a");
-//	ASSERT(MyString("ab").concat('c') == "abc");
-//	ASSERT(MyString("ab").concat("cde") == "abcde");
-//	ASSERT(s.concat("abc").concat(s).concat("") == "abc");
-//}
+void test_compare() {
+    MyString s("abc");
+    ASSERT_EQUAL(MyString().compareTo(""), 0);
+    ASSERT(s.compareTo("bcd") < 0);
+    ASSERT(s.compareTo("abde") < 0);
+    ASSERT(s.compareTo("abcd") < 0);
+    ASSERT(s.compareTo("abbb") > 0);
+    ASSERT(s.compareTo("ABC") > 0);
+    ASSERT(s.compareTo("") > 0);
+    ASSERT(MyString().compareTo(s) < 0);
+}
 
-void test_substring(){
+void test_concat() {
+    MyString s;
+    ASSERT(s.concat("") == "");
+    ASSERT(s.concat('a') == "a");
+    ASSERT(MyString("a").concat("") == "a");
+    ASSERT(MyString("ab").concat('c') == "abc");
+    ASSERT(MyString("ab").concat("cde") == "abcde");
+    ASSERT(s.concat("abc").concat(s).concat("") == "abc");
+}
+
+void test_substring() {
     MyString s("abcd");
-	ASSERT(s.substring(0, 2) == "ab");
-	ASSERT(s.substring(1, 3) == "bc");
-	ASSERT(s.substring(2, 4) == "cd");
-	ASSERT(s.substring(3, 5) == "d");
+    ASSERT(s.substring(0, 2) == "ab");
+    ASSERT(s.substring(1, 3) == "bc");
+    ASSERT(s.substring(2, 4) == "cd");
+    ASSERT(s.substring(3, 5) == "d");
 //	ASSERT(s.substring(0, -1) == s); should be ""
-	ASSERT(s.substring(2, 2) == "");
-	ASSERT(s.substring(5, 7) == "");
-	ASSERT(s.substring(3, 0) == "");
-	ASSERT(MyString().substring(0, 1) == "");
+    ASSERT(s.substring(2, 2) == "");
+    ASSERT(s.substring(5, 7) == "");
+    ASSERT(s.substring(3, 0) == "");
+    ASSERT(MyString().substring(0, 1) == "");
     MyString s2 = s.substring(2, 4);
-	ASSERT(s2.charAt(0) == 'c');
+    ASSERT(s2.charAt(0) == 'c');
 }
 
 //void test_valueOf(){
@@ -77,22 +77,24 @@ void test_substring(){
 //	ASSERT(MyString::valueOf(-10) == "-10");
 //	ASSERT(MyString::valueOf(-999) == "-999");
 //}
-//
-//void test_charAt(){
-//    MyString s("abc");
-//    ASSERT_EQUAL(s.charAt(0), 'a');
-//    ASSERT_EQUAL(s.charAt(1), 'b');
-//    ASSERT_EQUAL(s.charAt(2), 'c');
-//	bool catched = false;
-//	try { s.charAt(-1);
-//	} catch(std::exception&) { catched = true; }
-//	ASSERT(catched);
-//	catched = false;
-//	try { MyString("abc").charAt(3);
-//	} catch(std::exception&) { catched = true; }
-//	ASSERT(catched);
-//}
-//
+
+void test_charAt() {
+    MyString s("abc");
+    ASSERT_EQUAL(s.charAt(0), 'a');
+    ASSERT_EQUAL(s.charAt(1), 'b');
+    ASSERT_EQUAL(s.charAt(2), 'c');
+    bool catched = false;
+    try {
+        s.charAt(-1);
+    } catch (std::exception &) { catched = true; }
+    ASSERT(catched);
+    catched = false;
+    try {
+        MyString("abc").charAt(3);
+    } catch (std::exception &) { catched = true; }
+    ASSERT(catched);
+}
+
 //void test_toInt(){
 //	ASSERT(MyString("123").toInt() == 123);
 //	ASSERT(MyString("   123").toInt() == 123);
@@ -130,24 +132,24 @@ void test_substring(){
 
 //***** master & test registration**********************************************
 
-void runAllTests(int argc, char const *argv[]){
-	cute::suite s;
-	s.push_back(CUTE(test_length));
+void runAllTests(int argc, char const *argv[]) {
+    cute::suite s;
+    s.push_back(CUTE(test_length));
 //	s.push_back(CUTE(test_toCString));
-//	s.push_back(CUTE(test_equals));
-//	s.push_back(CUTE(test_compare));
-//	s.push_back(CUTE(test_concat));
-	s.push_back(CUTE(test_substring));
+    s.push_back(CUTE(test_equals));
+    s.push_back(CUTE(test_compare));
+    s.push_back(CUTE(test_concat));
+    s.push_back(CUTE(test_substring));
 //	s.push_back(CUTE(test_valueOf));
-//	s.push_back(CUTE(test_charAt));
+    s.push_back(CUTE(test_charAt));
 //	s.push_back(CUTE(test_toInt));
-	cute::xml_file_opener xmlfile(argc,argv);
-	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
-	cute::makeRunner(lis,argc,argv)(s, "AllTests");
+    cute::xml_file_opener xmlfile(argc, argv);
+    cute::xml_listener<cute::ide_listener<> > lis(xmlfile.out);
+    cute::makeRunner(lis, argc, argv)(s, "AllTests");
 }
 
-int main(int argc, char const *argv[]){
-    runAllTests(argc,argv);
+int main(int argc, char const *argv[]) {
+    runAllTests(argc, argv);
     return 0;
 }
 
